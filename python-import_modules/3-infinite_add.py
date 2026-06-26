fa06b4dc37fbac343fc00c18f3465c2b36b07d46
Anaise <a.umugwanez@alustend.com>
2026-06-26 18:16:32 +0200

    Added a Python guard for all question

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from sys import argv
-total = 0
-for i in range(1, len(argv)):
-    total += int(argv[i])
-print(total)
+if __name__ == "__main__":
+    total = 0
+    for i in range(1, len(argv)):
+        total += int(argv[i])
+    print(total)
--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-for name in dir(hidden_4):
-    if not name.startswith("__"):
-        print(name)
+if __name__ == "__main__":
+    for name in dir(hidden_4):
+        if not name.startswith("__"):
+            print(name)
