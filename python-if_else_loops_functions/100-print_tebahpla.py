8da1b5b619518b133a243626969ce0ce8119ade0
Anaise <a.umugwanez@alustend.com>
2026-06-26 17:59:42 +0200

    Fixed Q14

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(26):
-    print("{}".format(chr(122 - i) if i % 2 == 0
-          else chr(89 - i)), end="")
+    print("{}".format(chr(ord('z') - i) if i % 2 == 0
+          else chr(ord('Z') - i)), end="")
