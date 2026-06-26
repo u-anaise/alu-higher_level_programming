79b635b9cd5c50d6cf780e700a1b22ebef467840
Anaise <a.umugwanez@alustend.com>
2026-06-26 18:07:28 +0200

    Q0-Q5

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+n = len(argv) - 1
+if n == 0:
+    print("0 arguments.")
+elif n == 1:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+else:
+    print("{} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, argv[i]))
